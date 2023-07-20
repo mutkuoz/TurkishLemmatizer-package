dd1a4fff53ad1052d983f897acfaa61db2ddc018
Mehmet Utku "Alphard" Öztürk <98752107+thealphard@users.noreply.github.com>
2023-07-20 15:32:26 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0' 
+DESCRIPTION = 'Turkish lemmatizer'
+
+with open('README.md') as f:
+    long = f.read()
+setup(
+        name="trstemmer", 
+        version=VERSION,
+        author="Created by Abdullatif Köksal, packed by Mehmet Utku ÖZTÜRK",
+        author_email="<contact@ælphard.tk>",
+        description=DESCRIPTION,
+        long_description=long,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=[],
+        python_requires='>3.9.13',
+        keywords=['machine learning', 'nlp'],
+        classifiers= [
+            "Intended Audience :: Developers",
+            "Natural Language :: Turkish",
+            "Programming Language :: Python :: 3"
+        ]
+)
